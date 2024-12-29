fbf738d8edcb17302a732ae4a64806575f1aa68b
Andreaw1140 <154424498+Andreaw1140@users.noreply.github.com>
2024-12-29 22:49:34 +0700

    Update Zemito-Hub.lua

--- Zemito-Hub.lua
+++ Zemito-Hub.lua
@@ -1,5 +1,5 @@
 -- Load KeceHub Library
-local KeceHub = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
+local KeceHub = loadstring(game:HttpGet("https://raw.githubusercontent.com/Andreaw1140/Andretremor-HUB/refs/heads/main/source"))()
 
 -- Tema Frost
 KeceHub.Themes["FrostTheme"] = {
